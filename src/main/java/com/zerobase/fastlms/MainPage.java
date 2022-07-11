855bb04f4d1c5d2f7848618aa23468189798a1ed
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-07-11 16:26:35 +0900

    Index 및 hello Page

---
+++ src/main/java/com/zerobase/fastlms/MainPage.java
@@ -0,0 +1,29 @@
+package com.zerobase.fastlms;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class MainPage {
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "Index Page";
+    }
+    @RequestMapping("/")
+    public String hello(HttpServletRequest request, HttpServletResponse response) {
+
+        String msg = "<html>" +
+                "<head>" +
+                "</head>" +
+                "<body>" +
+                "<p>hello</p> <p>fastlms website!!!</p>" +
+                "</body>" +
+                "</html>";
+        return "Index Page";
+    }
+}
