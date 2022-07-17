8d279e1ee1b6640ea03b213fa35233b7d3808e9a
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-07-18 01:50:05 +0900

    2022-07-18 (Chapter 12 : 수강 완료)

---
+++ src/main/java/com/zerobase/fastlms/admin/dto/MemberDto.java
@@ -0,0 +1,54 @@
+package com.zerobase.fastlms.admin.dto;
+
+import com.zerobase.fastlms.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class MemberDto {
+
+    String userId;
+    String password;
+    String phone;
+    LocalDateTime regDt;
+    String userName;
+
+    String emailAuthKey;
+    boolean emailAuthYn;
+    LocalDateTime emailAuthDt;
+
+    String resetPasswordKey;
+    LocalDateTime resetPasswordLimitDt;
+
+    boolean adminYn;
+    String userStatus;
+
+    //추가 컬럼
+    long totalCount;
+    long seq;
+
+    public static MemberDto of(Member member) {
+        return MemberDto.builder()
+                .userId(member.getUserId())
+                .userName(member.getUserName())
+                .phone(member.getPhone())
+//                .password(member.getPassword())
+                .regDt(member.getRegDt())
+                .emailAuthYn(member.isEmailAuthYn())
+                .emailAuthDt(member.getEmailAuthDt())
+                .emailAuthKey(member.getEmailAuthKey())
+                .resetPasswordKey(member.getResetPasswordKey())
+                .resetPasswordLimitDt(member.getResetPasswordLimitDt())
+                .adminYn(member.isAdminYn())
+                .userStatus(member.getUserStatus())
+                .build();
+    }
+
+}
